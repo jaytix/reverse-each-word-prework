21c181e7fb3d2b2bd2201e7d0021a5a390cd2694
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-16 20:42:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
-def reverse_each_word(sentence)
-  arr = sentence.split(" ")
-  arr.each do |word|
-    word.reverse!
-  end
-  arr.join(" ")
-end
+#def reverse_each_word(sentence)
+#  arr = sentence.split(" ")
+#  arr.each do |word|
+#    word.reverse!
+#  end
+#  arr.join(" ")
+#end
 
 def reverse_each_word(sentence)
   arr = sentence.split(" ")
   arr.collect do |word|
-    word.reverse
+    word.reverse!
   end
   arr.join(" ")
 end
